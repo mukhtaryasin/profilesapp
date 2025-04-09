4ddfd8234c441564e87333c99c4ddd8d49e3ab27
Mukhtar Ahmed Yasin <mayasin252@gmail.com>
2025-04-10 01:21:50 +1000

    updated sandbox and redployed

--- profilesapp/vite.config.js
+++ profilesapp/vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Enable React Fast Refresh
-  server: {
-    port: 3000, // Change this to your desired port
-    open: true, // Automatically open the browser
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,  // Clear the output directory before building
   },
-  base: './', // This is important for the app to work in the Amplify Console
 })
