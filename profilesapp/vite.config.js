b83c04796c673ff36c090f02881c24e066ab971e
Mukhtar Ahmed Yasin <mayasin252@gmail.com>
2025-04-10 01:37:43 +1000

    updated sandbox and redployed

--- profilesapp/vite.config.js
+++ profilesapp/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: 'profilesapp/dist',
     emptyOutDir: true,  // Clear the output directory before building
   },
 })
