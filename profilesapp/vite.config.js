2e528ba7303f9dfb65cf1f25fc0957af702b9472
Mukhtar Ahmed Yasin <mayasin252@gmail.com>
2025-04-09 23:31:45 +1000

    added user profiles

--- profilesapp/vite.config.js
+++ profilesapp/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Enable React Fast Refresh
+  server: {
+    port: 3000, // Change this to your desired port
+    open: true, // Automatically open the browser
+  },
   base: './', // This is important for the app to work in the Amplify Console
 })
